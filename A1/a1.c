5592405a413005d168d4c1a4d3d335ed712c96fc
Rafiu Hossain <hossainrafiu@gmail.com>
2022-02-23 13:32:47 -0500

    A1 Starter Code

---
+++ A1/a1.c
@@ -0,0 +1,54 @@
+#include "a1.h"
+
+/**
+	Add your functions to this file.
+	Make sure to review a1.h.
+	Do NOT include a main() function in this file
+	when you submit.
+*/
+
+
+void print_menu(Menu* menu){
+	fprintf(stdout, "--- Menu ---\n");
+	for (int i = 0; i < menu->num_items; i++){
+		fprintf(stdout, "(%s) %s: %.2f\n", 
+			menu->item_codes[i], 
+			menu->item_names[i], 
+			menu->item_cost_per_unit[i]	
+		);
+	}
+}
+
+
+void print_order(Order* order){
+	for (int i = 0; i < order->num_items; i++){
+		fprintf(
+			stdout, 
+			"%d x (%s)\n", 
+			order->item_quantities[i], 
+			order->item_codes[i]
+		);
+	}
+}
+
+
+void print_receipt(Order* order, Menu* menu){
+	for (int i = 0; i < order->num_items; i++){
+		double item_cost = get_item_cost(order->item_codes[i], menu);
+		fprintf(
+			stdout, 
+			"%d x (%s)\n @$%.2f ea \t %.2f\n", 
+			order->item_quantities[i],
+			order->item_codes[i], 
+			item_cost,
+			item_cost * order->item_quantities[i]
+		);
+	}
+	double order_subtotal = get_order_subtotal(order, menu);
+	double order_total = get_order_total(order, menu);
+	
+	fprintf(stdout, "Subtotal: \t %.2f\n", order_subtotal);
+	fprintf(stdout, "               -------\n");
+	fprintf(stdout, "Tax %d%%: \t$%.2f\n", TAX_RATE, order_total);
+	fprintf(stdout, "              ========\n");
+}
